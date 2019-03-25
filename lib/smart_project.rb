a28dcc131eddca716cfa72139d4006e46f689e5e
Marco Iannantuono <marco.iannantuono@democom.it>
2019-03-25 18:14:47 +0100

    vediamo se va

--- lib/smart_project.rb
+++ lib/smart_project.rb
@@ -2,4 +2,17 @@ require "smart_project/version"
 
 module SmartProject
   class Error < StandardError; end
+  autoload :AccountService,     'smart_project/account_service'
+  autoload :Authentication,     'smart_project/authentication'
+  autoload :Error,              'smart_project/error'
+  autoload :Delegator,          'smart_project/delegator'
+
+  module Helpers
+    autoload :Session,          'smart_project/helpers/session'
+  end
+
+  module Strategies
+    autoload :Delegator,        'smart_project/strategies/session'
+    autoload :Delegator,        'smart_project/strategies/token'
+  end
 end
