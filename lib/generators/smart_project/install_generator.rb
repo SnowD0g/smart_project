9f2aff05cce03dee0210b269fb7c910415aadedb
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-26 15:19:20 +0100

    Update install_generator.rb

--- lib/generators/smart_project/install_generator.rb
+++ lib/generators/smart_project/install_generator.rb
@@ -8,12 +8,35 @@ module SmartProject
 
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../../../../", __FILE__)
-      def test
-        puts 'ciaooo'
-      end
-      
-      def asd
-        puts 'asdasd'
+
+      def configure_application
+        application_type = ask('Tipo di applicazione [a]pi / [w]eb ?')
+
+        copy_file 'config/endpoints.yml', 'config/endpoints.yml'
+        copy_file 'config/initializers/warden.rb', 'config/initializers/warden.rb'
+        append_to_file 'app/controllers/application.rb', 'include SmartProject::Helpers::Session'
+        append_to_file 'app/controllers/application.rb', 'config.endpoints = config_for(:endpoints)'
+        case application_type
+        when 'a'
+        content = <<-RUBY
+            config.middleware.use Warden::Manager do |manager|
+              manager.default_strategies :token
+              manager.failure_app = ->(env){ SmartProject::Error::UnauthorizedApiController.action(:index).call(env) }
+            end
+        RUBY
+        append_to_file 'config/application.rb', content
+        append_to_file 'config/initializers/warden.rb', 'Warden::Strategies.add(:session, SmartProject::Strategies::Session)'
+        append_to_file 'app/controllers/application.rb', 'skip_before_action :verify_authenticity_token'
+        when 'w'
+        content = <<-RUBY
+          config.middleware.use Warden::Manager do |manager|
+            manager.default_strategies :session
+            manager.failure_app = ->(env){ SmartProject::Error::UnauthorizedWebController.action(:index).call(env) }
+          end
+        RUBY
+        append_to_file 'config/application.rb', content
+        append_to_file 'config/initializers/warden.rb', 'Warden::Strategies.add(:token, SmartProject::Strategies::Token)'
+        end
       end
     end
   end
