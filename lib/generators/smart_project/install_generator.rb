bf6e7796dacba4dd0230e258d46683837b99b519
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-26 15:04:00 +0100

    Update install_generator.rb

--- lib/generators/smart_project/install_generator.rb
+++ lib/generators/smart_project/install_generator.rb
@@ -11,6 +11,13 @@ module SmartProject
 
       mattr_accessor :application_type
       @@application_type = ask_with_default('Tipo di applicazione [a]pi / [w]eb', 'w')
+      
+      def ask_with_default(question, default, color = :blue)
+        return default unless $stdin.tty?
+        question = (question.split("?") << " [#{default}]?").join
+        answer = ask(question, color)
+        answer.to_s.strip.empty? ? default : answer
+      end
     end
   end
 end
