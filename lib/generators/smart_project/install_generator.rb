5281ed0f0bde7c36c58e6f7e70dca8fccbb4cac8
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-26 15:10:45 +0100

    Update install_generator.rb

--- lib/generators/smart_project/install_generator.rb
+++ lib/generators/smart_project/install_generator.rb
@@ -8,14 +8,8 @@ module SmartProject
 
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../../../../", __FILE__)
-
-      @@application_type = ask_with_default('Tipo di applicazione [a]pi / [w]eb', 'w')
-      
-      def ask_with_default(question, default, color = :blue)
-        return default unless $stdin.tty?
-        question = (question.split("?") << " [#{default}]?").join
-        answer = ask(question, color)
-        answer.to_s.strip.empty? ? default : answer
+      def test
+        puts 'ciaooo'
       end
     end
   end
