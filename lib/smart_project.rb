fc72f67c89b6347d948f75dba3c1cce6d6c772f7
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-26 10:34:13 +0100

    Update smart_project.rb

--- lib/smart_project.rb
+++ lib/smart_project.rb
@@ -1,19 +1,22 @@
 require "smart_project/version"
 
 module SmartProject
-  class Error < StandardError; end
-  autoload :AccountService,     'smart_project/account_service'
-  autoload :Authentication,     'smart_project/authentication'
-  autoload :Error,              'smart_project/error'
-  autoload :Delegator,          'smart_project/delegator'
+  autoload :AccountService, 'smart_project/account_service'
+  autoload :Authentication, 'smart_project/authentication'
+  autoload :Error, 'smart_project/error'
+  autoload :Delegator, 'smart_project/delegator'
 
   module Helpers
-    autoload :Session,             'smart_project/helpers/session'
+    autoload :Session, 'smart_project/helpers/session'
   end
 
   module Strategies
-    autoload :Session,          'smart_project/strategies/session'
-    autoload :Token,            'smart_project/strategies/token'
+    autoload :Session, 'smart_project/strategies/session'
+    autoload :Token, 'smart_project/strategies/token'
+  end
+  
+  module Error
+    autoload UnauthorizedWebController, 'smart_project/strategies/session'
   end
   
   mattr_accessor :test
