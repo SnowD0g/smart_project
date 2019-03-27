5afb7762db52e00b7640611f4ac14e64066c88a8
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-27 17:51:19 +0100

    Update session.rb

--- lib/smart_project/strategies/session.rb
+++ lib/smart_project/strategies/session.rb
@@ -7,18 +7,22 @@ module SmartProject::Strategies
     end
 
     def authenticate!
-      user = "SmartProject::Authentication::#{payload['type']}".constantize.new(payload)
+      begin
+        user = "SmartProject::Authentication::#{payload['type']}".constantize.new(payload)
+      rescue
+        raise SmartProject::Error::Unauthorized
+      end
       user.nil? ? fail!('strategies.session.failed') : success!(user)
     end
 
     private
 
     def authentication_session
-      session.has_key?(:token_payload)
+      session.has_key?('warden.user.default.key') || params.has_key?(:session)
     end
 
     def payload
-      HashWithIndifferentAccess.new(session[:token_payload])
+      HashWithIndifferentAccess.new(session['warden.user.default.key'])
     end
   end
 end
