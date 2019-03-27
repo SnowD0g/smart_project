6ea410ea0bc059b2bcd45cb6bc2175ed6e3daa6a
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-27 12:29:54 +0100

    Update session.rb

--- lib/smart_project/strategies/session.rb
+++ lib/smart_project/strategies/session.rb
@@ -5,9 +5,8 @@ module SmartProject::Strategies
     end
 
     def authenticate!
-      user_payload.nil? ? fail!('strategies.session.failed') : success!(user_payload)
-      # user = "SmartProject::Authentication::#{payload['type']}".constantize.new(payload)
-      # user.nil? ? fail!('strategies.session.failed') : success!(user)
+      user = "SmartProject::Authentication::#{payload['type']}".constantize.new(payload)
+      user.nil? ? fail!('strategies.session.failed') : success!(user)
     end
 
     private
@@ -16,7 +15,7 @@ module SmartProject::Strategies
       session.has_key?(:token_payload)
     end
 
-    def user_payload
+    def payload
       HashWithIndifferentAccess.new(session[:token_payload])
     end
   end
